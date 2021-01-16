29f1c59063de5a8217588d17e769f049bf37bdbb
Kevin <kevinjoseph97@gmail.com>
2021-01-15 21:43:46 -0500

    commit tree a96620ce62f97524c206c3b8df507bbb3aa34db3

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
+# def square_array(array)
+#   ar_new = []
+#   array.each do |number|
+#     ar_new! {|number| array**2}
+
+#   end
+
+# end
+
 def square_array(array)
-  # your code here
+  arr_new = []
+  array.each { |array| arr_new << array ** 2 }
+  arr_new
 end
